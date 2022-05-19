f7d8122e3ecf6f563d35b9959153f664febe89af
d00605132 <dingxiaowei2@huawei.com>
2022-05-19 16:31:29 +0800

    添加文件上传案例

--- MonitorToolSystem/MonitorToolSystem/PostFileHandler.ashx.cs
+++ MonitorToolSystem/MonitorToolSystem/PostFileHandler.ashx.cs
@@ -1,40 +1,128 @@
 ﻿using System;
+using System.IO;
 using System.Web;
 
 namespace MonitorToolSystem
 {
+    public class Result
+    {
+        /// <summary>
+        /// 无参构造函数. Success属性初始值默认为true, DefaultErrMsg初始默认为 '发生错误，请联系系统管理员！'
+        /// </summary>
+        public Result()
+        {
+            this.Success = true;
+            this.DefaultErrMsg = "发生错误，请联系系统管理员！";
+        }
+
+        /// <summary>
+        /// 返回结果是否为成功
+        /// </summary>
+        public bool Success { get; set; }
+
+        /// <summary>
+        /// 错误信息
+        /// </summary>
+        public string ErrMsg { get; set; }
+
+        /// <summary>
+        /// 缺省错误信息( "发生错误，请联系系统管理员！" )
+        /// </summary>
+        public string DefaultErrMsg { get; set; }
+
+        /// <summary>
+        /// 其它信息
+        /// </summary>
+        public string OtherInfo { get; set; }
+
+        /// <summary>
+        /// 插入或者更新时的主键
+        /// </summary>
+        public string PkId { get; set; }
+
+        /// <summary>
+        /// 对象，用于扩展（任意情况）
+        /// </summary>
+        public Object ExtInfo { get; set; }
+    }
     /// <summary>
     /// PostFileHandler 的摘要说明
     /// </summary>
     public class PostFileHandler : IHttpHandler
     {
+        #region [ 属性 ]
+        public HttpRequest Request
+        {
+            get
+            {
+                return HttpContext.Current.Request;
+            }
+        }
+
+        public HttpServerUtility Server
+        {
+            get
+            {
+                return HttpContext.Current.Server;
+            }
+        }
+        #endregion
+
         public void ProcessRequest(HttpContext context)
         {
             context.Response.ContentType = "text/plain";
-            string msg = string.Empty;
-            string error = string.Empty;
-            string result = string.Empty;
-            string filePath = string.Empty;
-            string fileNewName = string.Empty;
+            string json = string.Empty;
+            Result r = new Result();
+            r.Success = false;
+            r.ErrMsg = "没有上传文件";
 
-            //这里只能用<input type="file" />才能有效果,因为服务器控件是HttpInputFile类型
-            HttpFileCollection files = context.Request.Files;
-            if (files.Count > 0)
+            try
             {
-                //设置文件名
-                fileNewName = DateTime.Now.ToString("yyyyMMddHHmmssff") + "_" + System.IO.Path.GetFileName(files[0].FileName);
-                //保存文件
-                files[0].SaveAs(context.Server.MapPath("../Upload/" + fileNewName));
-                msg = "文件上传成功！";
-                result = "{msg:'" + msg + "',filenewname:'" + fileNewName + "'}";
+                if (Request.Files.Count > 0)
+                {
+                    HttpPostedFile file = Request.Files[0];
+                    if (file.ContentLength == 0)
+                    {
+                        r.Success = false;
+                        r.ErrMsg = "文件长度为0";
+                    }
+                    else
+                    {
+                        r.OtherInfo = file.ContentLength.ToString();
+                        string uploadDir = Server.MapPath("~\\Upload");
+                        if (!Directory.Exists(uploadDir))
+                        {
+                            Directory.CreateDirectory(uploadDir);
+                        }
+
+                        string filePath = Path.Combine(uploadDir, Path.GetFileName(file.FileName));
+                        if (File.Exists(filePath))
+                        {
+                            try
+                            {
+                                File.Delete(filePath);
+                            }
+                            catch (Exception ex)
+                            {
+                                System.Threading.Thread.Sleep(1000);
+                                filePath = Path.Combine(uploadDir, DateTime.Now.ToString("yyyy-MM-dd_HHmmss_") + Path.GetFileName(file.FileName));
+                            }
+                        }
+
+                        file.SaveAs(filePath);
+                        r.Success = true;
+                    }
+                }
             }
-            else
+            catch (Exception ex)
             {
-                error = "文件上传失败！";
-                result = "{ error:'" + error + "'}";
+                r.Success = false;
+                r.ErrMsg = ex.Message;
             }
-            context.Response.Write(result);
-            context.Response.End();
+
+            //输出结果json
+            json = Newtonsoft.Json.JsonConvert.SerializeObject(r);
+            context.Response.Write(json);
         }
 
         public bool IsReusable
