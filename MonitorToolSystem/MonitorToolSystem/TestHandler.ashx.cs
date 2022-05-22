aa174c2dde489bf18d74ad7ee13e76a8fcee61ce
d00605132 <dingxiaowei2@huawei.com>
2022-05-22 23:03:40 +0800

    添加测试

---
+++ MonitorToolSystem/MonitorToolSystem/TestHandler.ashx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace MonitorToolSystem
+{
+    /// <summary>
+    /// TestHandler 的摘要说明
+    /// </summary>
+    public class TestHandler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            string id = context.Request.Form["id"];
+            HttpPostedFile hfc = context.Request.Files["Photo"];
+            if (hfc == null) return;
+
+            Stream sm = hfc.InputStream;
+            byte[] buffer = new byte[sm.Length];
+            sm.Read(buffer, 0, buffer.Length);
+            sm.Close();
+            sm.Dispose();
+            string uploadDir = HttpContext.Current.Server.MapPath("~\\Upload");
+            if (!Directory.Exists(uploadDir))
+            {
+                Directory.CreateDirectory(uploadDir);
+            }
+            string path = Path.Combine(uploadDir, $"{id}\\");
+            //判断路径是否存在
+            if (!Directory.Exists(path))
+            {
+                //如果不存在就创建
+                Directory.CreateDirectory(path);
+            }
+            //产生文件名
+            string fileName = path + id + "_" + DateTime.Now.ToString("yyyy-MM-dd_hh-mm-ss") + "_" + DateTime.Now.Millisecond.ToString() + ".png";
+            Stream flstr = new FileStream(fileName, FileMode.Create);
+            BinaryWriter sw = new BinaryWriter(flstr, Encoding.Unicode);
+            sw.Write(buffer);
+            flstr.Close();
+            sw.Close();
+            sw.Dispose();
+            flstr.Dispose();
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
