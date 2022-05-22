eaf744df8e18c121918b17c198c734890b91bbfc
dingxiaowei <1213250243@qq.com>
2022-05-22 23:28:50 +0800

    添加测试文件上传  跑通

--- MonitorToolSystem/MonitorToolSystem/TestHandler.ashx.cs
+++ MonitorToolSystem/MonitorToolSystem/TestHandler.ashx.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
+﻿using System.IO;
 using System.Web;
 
 namespace MonitorToolSystem
@@ -16,36 +12,23 @@ namespace MonitorToolSystem
         public void ProcessRequest(HttpContext context)
         {
             context.Response.ContentType = "text/plain";
-            string id = context.Request.Form["id"];
-            HttpPostedFile hfc = context.Request.Files["Photo"];
-            if (hfc == null) return;
-
-            Stream sm = hfc.InputStream;
-            byte[] buffer = new byte[sm.Length];
-            sm.Read(buffer, 0, buffer.Length);
-            sm.Close();
-            sm.Dispose();
+            string name = context.Request.Form["testcase_name"];
+            HttpPostedFile file = context.Request.Files["folder"];
+            if (file == null) return;
             string uploadDir = HttpContext.Current.Server.MapPath("~\\Upload");
             if (!Directory.Exists(uploadDir))
             {
                 Directory.CreateDirectory(uploadDir);
             }
-            string path = Path.Combine(uploadDir, $"{id}\\");
+            string path = Path.Combine(uploadDir, $"{name}\\");
             //判断路径是否存在
             if (!Directory.Exists(path))
             {
                 //如果不存在就创建
                 Directory.CreateDirectory(path);
             }
-            //产生文件名
-            string fileName = path + id + "_" + DateTime.Now.ToString("yyyy-MM-dd_hh-mm-ss") + "_" + DateTime.Now.Millisecond.ToString() + ".png";
-            Stream flstr = new FileStream(fileName, FileMode.Create);
-            BinaryWriter sw = new BinaryWriter(flstr, Encoding.Unicode);
-            sw.Write(buffer);
-            flstr.Close();
-            sw.Close();
-            sw.Dispose();
-            flstr.Dispose();
+            file.SaveAs(path + name + $"{file.FileName}");
+            context.Response.Write("ok");
         }
 
         public bool IsReusable
